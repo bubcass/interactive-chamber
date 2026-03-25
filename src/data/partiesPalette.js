3fa91fdeca1b2534a2d61b4ca343b9f5594dc864
bubcass <81823823+bubcass@users.noreply.github.com>
2026-03-25 08:02:10 +0000

    Update party colours palette to match site

--- src/data/partiesPalette.js
+++ src/data/partiesPalette.js
@@ -1,15 +1,15 @@
 export const partiesPalette = [
-  { name: "Fianna Fáil", value: "#40b34e" },
+  { name: "Fianna Fáil", value: "#2c8737" },
   { name: "Sinn Féin", value: "#088460" },
   { name: "Fine Gael", value: "#303591" },
   { name: "Independent", value: "#666666" },
   { name: "Labour Party", value: "#c82832" },
   { name: "Social Democrats", value: "#782b81" },
-  { name: "Independent Ireland", value: "#17becf" },
-  { name: "People Before Profit-Solidarity", value: "#c5568b" },
-  { name: "Aontú", value: "#ff7f0e" },
+  { name: "Independent Ireland", value: "#087b87" },
+  { name: "People Before Profit-Solidarity", value: "#be417d" },
+  { name: "Aontú", value: "#b35400" },
   { name: "100% RDR", value: "#985564" },
-  { name: "Green Party", value: "#b4d143" },
+  { name: "Green Party", value: "#6c7e26" },
 ];
 
 export const partyColorMap = Object.fromEntries(
